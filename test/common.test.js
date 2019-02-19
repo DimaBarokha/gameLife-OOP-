0a619c5a3c09133e1bbb225e0ce473d455147fa3
dbarokha <dima@barokha.com>
2019-02-19 16:35:26 +0300

    first commit

---
+++ test/common.test.js
@@ -0,0 +1,8 @@
+const sum = require('../src/js/common');
+/*eslint-disable */
+describe('Summarizing', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
+/* eslint-enable */
